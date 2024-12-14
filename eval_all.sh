72b7e5e02993b241e1ed82e49cf810ec86b09be7
zhaoziyu.styx <zhaoziyu.styx@bytedance.com>
2024-12-14 12:54:16 +0800

    init commit

--- eval_all.sh
+++ eval_all.sh
@@ -1,5 +1,5 @@
-python main.py --data_path  dataset/combined_test.json  --res_path results/misture.json --eval_type mixture  --lora_num 3 --batch_size 8
-python main.py --data_path  dataset/combined_test.json  --res_path results/misture_ood.json --eval_type mixture  --lora_num 3 --batch_size 8 --ood=True
+python main.py --data_path  dataset/combined_test.json  --res_path results/mixture.json --eval_type mixture  --lora_num 3 --batch_size 8
+python main.py --data_path  dataset/combined_test.json  --res_path results/mixture_ood.json --eval_type mixture  --lora_num 3 --batch_size 8 --ood=True
 python main.py --data_path  dataset/combined_test.json  --res_path results/fusion.json --eval_type fusion  --lora_num 3 --batch_size 8
 python main.py --data_path  dataset/combined_test.json  --res_path results/fusion_ood.json --eval_type fusion  --lora_num 3 --batch_size 8 --ood=True
 python main.py --data_path  dataset/combined_test.json  --res_path results/selection.json --eval_type fusion  --lora_num 1 --batch_size 8
